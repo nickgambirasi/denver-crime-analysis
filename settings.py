d141c8b34856035a3ddb6a89665a942b5742c49f
Nick <nickgambirasi@gmail.com>
2023-09-29 14:36:43 -0600

    settings updates for the data collection function

---
+++ settings.py
@@ -0,0 +1,15 @@
+"""
+Developer Info
+--------------
+Developed By: Nicholas Gambirasi
+Created On: 29 September 2023
+Last Edited: 29 September 2023
+
+Purpose
+--------------
+This file contains user settings that can alter where and how
+this project collects and analyzes its data.
+"""
+import os
+
+DATA_DIR = "./data"
